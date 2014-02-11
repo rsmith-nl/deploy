08a2ec3752e43231703e1c210df33cb75a609965
Roland Smith <rsmith@xs4all.nl>
2014-02-12 00:28:52 +0100

    Port check and install to python.

---
+++ check.py
@@ -0,0 +1,102 @@
+# /usr/bin/env python3 vim:fileencoding=utf-8
+#
+# Author: R.F. Smith <rsmith@xs4all.nl> Created: 2013-11-20 22:08:38 +0100
+# Modified: $Date$
+#
+# To the extent possible under law, R.F. Smith has waived all copyright and
+# related or neighboring rights to check.py. This work is published from the
+# Netherlands. See http://creativecommons.org/publicdomain/zero/1.0/
+
+"""Script to chech which files need to be installed."""
+
+import sys
+import os
+import subprocess
+
+__version__ = '$Revision$'[11:-2]
+
+
+def parse(lines):
+    """Generator to parse a list of lines from a filelist.USER file. The lines
+    in these files have a special format. Lines where the first non-whitespace
+    character is a '#' are ignored, as are blank lines and lines that have less
+    than three items.
+
+    Lines should start with a source file name, then a permission for the
+    installed file and then the destination file name. The rest of the line is
+    a post-install command.
+
+    :param lines: A list of lines
+    :yields: A tuple (src, perm, dest, commands)
+    """
+    for ln in lines:
+        items = ln.split(maxsplit=3)
+        if len(items) < 3:
+            continue
+        if len(items) > 3:
+            src, perm, dest, rem = items
+            if src.startswith('#'):
+                continue
+            cmds = rem.split()
+        else:
+            cmds = None
+            src, perm, dest = items
+        yield src, perm, dest, cmds
+
+
+def identical(src, dest):
+    """Test whether two files are identical.
+
+    :param src: Path of the source file.
+    :param dest: Path of the destination file.
+    :returns: True if src and dest are the same, False otherwise.
+    """
+    csrc = subprocess.check_output(['sha256', '-q', src])
+    cdest = subprocess.check_output(['sha256', '-q', dest])
+    if csrc != cdest:
+        return False
+    else:
+        return True
+
+
+def ansiprint(s, fg='', bg=''):
+    """Prints a text with ansi colors.
+
+    :param fg: optional foreground color.
+    :param fg: optional background color.
+    """
+    if fg:
+        fg = '\033[{:d}m'.format(fg)
+    if bg:
+        bg = '\033[{:d}m'.format(bg)
+    print(''.join([fg, s, bg, '\033[0m']))
+
+
+def main(argv):
+    """Entry point for this script.
+    """
+    diffs = '-d' in argv
+    verbose = '-v' in argv
+    filelist = '.'.join(['filelist', os.environ['USER']])
+    try:
+        with open(filelist, 'r') as input:
+            lines = input.readlines()
+    except IOError as e:
+        print(e)
+        sys.exit(1)
+    for src, perm, dest, cmds in parse(lines):
+        if not os.path.exists(dest):
+            ansiprint("'{}' does not exist.".format(dest), fg=30, bg=41)
+            continue
+        if not identical(src, dest):
+            if diffs:
+                args = ['diff', '-u', '-d', src, dest]
+                print(subprocess.check_output(args))
+            elif verbose:
+                ansiprint("'{}' differs from '{}'.".format(src, dest), fg=31)
+        elif verbose:
+            ansiprint("'{}' and '{}' are the same.".format(src, dest), fg=32)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ install.py
@@ -0,0 +1,49 @@
+# /usr/bin/env python3 vim:fileencoding=utf-8
+#
+# Author: R.F. Smith <rsmith@xs4all.nl> Created: 2013-11-20 22:08:38 +0100
+# Modified: $Date$
+#
+# To the extent possible under law, R.F. Smith has waived all copyright and
+# related or neighboring rights to install.py. This work is published from the
+# Netherlands. See http://creativecommons.org/publicdomain/zero/1.0/
+
+"""Script to chech which files need to be installed."""
+
+import sys
+import os
+import subprocess
+import check
+
+
+__version__ = '$Revision$'[11:-2]
+
+
+def main(argv):
+    """Entry point for this script.
+    """
+    verbose = '-v' in argv
+    filelist = '.'.join(['filelist', os.environ['USER']])
+    try:
+        with open(filelist, 'r') as input:
+            lines = input.readlines()
+    except IOError as e:
+        print(e)
+        sys.exit(1)
+    for src, perm, dest, cmds in check.parse(lines):
+        if not check.identical(src, dest):
+            try:
+                os.replace(src, dest)
+                os.chmod(dest, perm)
+                if subprocess.call(cmds) is not 0:
+                    s = 'Post-install commands for {} failed.'.format(dest)
+                    check.ansiprint(s, fg=31)
+            except Exception as e:
+                check.ansiprint(e, fg=31)
+                continue
+        if verbose:
+            s = "'{}' successfully installed as '{}'.".format(src, dest)
+            check.ansiprint(s, fg=32)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
